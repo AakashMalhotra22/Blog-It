1df5e872be4bf7370460d8bd28df293b80e86ca3
aakashmalhotra22 <aakashmalhotra2863@gmail.com>
2023-04-06 00:51:03 +0530

    creting home page

--- client-frontend/src/App.js
+++ client-frontend/src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import f_p from './f_p.jpg'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+    <main>
+      <header>
+        <a href="" className="logo"> MyBlog</a>
+        <nav>
+          <a href="">Login</a>
+          <a href="">Register</a>
+        </nav>
       </header>
-    </div>
+      <div className="post">
+        <img src={f_p} height ="300px" width ="300"/>
+        <div className = "texts">
+          <h2>This is title</h2>
+          <p>this is para</p>
+        </div>
+      </div>
+
+      <div className="post">
+        <img src={f_p} height ="300px" width ="300"/>
+        <div className = "texts">
+          <h2>This is title</h2>
+          <p>this is para</p>
+        </div>
+      </div>
+      <div className="post">
+        <img src={f_p} height ="300px" width ="300"/>
+        <div className = "texts">
+          <h2>This is title</h2>
+          <p>this is para</p>
+        </div>
+      </div>
+
+      
+     
+    </main>
+    </>
   );
 }
 
