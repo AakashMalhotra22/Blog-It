0aece0173454b3ec1605a1c2b52f3e8aebe346cd
aakashmalhotra22 <aakashmalhotra2863@gmail.com>
2023-04-20 13:18:32 +0530

    indexPage changed

--- client-frontend/src/Components/CreatePost.js
+++ client-frontend/src/Components/CreatePost.js
@@ -43,6 +43,7 @@ const CreatePost = ()=>{
         // if properly works, navigate to blog page
         if (response.ok) 
         {
+            console.log(response.status);
           navigate("/");
         }
         // Ask to login again
@@ -59,7 +60,7 @@ const CreatePost = ()=>{
             <input type="title" placeholder={'Title'} value={title} onChange={ev => setTitle(ev.target.value)} />
             <input type="summary" placeholder={'Summary'} value={summary} onChange={ev => setSummary(ev.target.value)} />
             <input type="file" onChange={ev => setFiles(ev.target.files)} />
-            <ReactQuill/>
+            <ReactQuill value ={content} modules = {modules} onChange={newValue=>setContent(newValue)}/>
             <button style={{marginTop:'5px'}}>Create Post</button>
         </form>
     )
